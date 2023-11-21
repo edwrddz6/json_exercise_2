8b4738d8e5d2ce60e25da9a911da18c26b1e8898
Edward Diaz <diazedward24@gmail.com>
2023-11-20 19:05:04 -0800

    Add exercise code, add JSON.stringify

---
+++ script.js
@@ -0,0 +1,20 @@
+const dataObject = {
+    user: {
+        id: 1,
+        name: "John Doe",
+        email: "john@example.com",
+        address: {
+            city: "New York",
+            zipcode: "10001"
+        }
+    },
+    product: {
+        id: 101,
+        name: "Laptop",
+        price: 999.99
+    }
+};
+
+const data = JSON.stringify(dataObject);
+
+console.log(data);
